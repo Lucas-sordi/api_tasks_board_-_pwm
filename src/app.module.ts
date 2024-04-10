d87ba7b209e800fd1409ba47d137f6cdfc2d73cb
Lucas <lucas.sordi@compasso.com.br>
2024-04-10 14:13:39 -0300

    conecta com banco postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    ConfigModule.forRoot({
+    envFilePath: ['.env'],
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+  }),
+  TaskModule],
   controllers: [],
   providers: [],
 })
