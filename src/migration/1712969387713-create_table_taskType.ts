2d2f5f1f758afe3aeb9aec657c5f00f4c76adf74
Lucas <lucas.sordi@compasso.com.br>
2024-04-12 22:38:52 -0300

    ajuste migration criacao tabela taskType

--- src/migration/1712969387713-create_table_taskType.ts
+++ src/migration/1712969387713-create_table_taskType.ts
@@ -7,9 +7,8 @@ export class CreateTableTaskType1712969387713 implements MigrationInterface {
         await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS public."taskType"
             (
-                id integer NOT NULL DEFAULT nextval('taskType_id_seq'),
-                type character varying COLLATE pg_catalog."default" NOT NULL,
-                CONSTRAINT "PK_97d94edc34bd1a1d20fe621f581" PRIMARY KEY (id)
+                id SERIAL PRIMARY KEY,
+                type character varying COLLATE pg_catalog."default" NOT NULL
             )
         `);
 
