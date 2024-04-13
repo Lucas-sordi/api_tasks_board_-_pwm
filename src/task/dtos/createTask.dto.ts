a18bce10ed135a7a7afb37a4ab3f58377c73e134
Lucas <lucas.sordi@compasso.com.br>
2024-04-13 00:05:44 -0300

    implementa validacoes

--- src/task/dtos/createTask.dto.ts
+++ src/task/dtos/createTask.dto.ts
@@ -1,6 +1,17 @@
-export interface CreateTaskDTO {
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateTaskDTO {
+
+    @IsString()
     name: string;
+
+    @IsString()
     description: string;
+
+    @IsNumber()
+    @IsOptional()
     parentId: number;
+
+    @IsNumber()
     typeId: number;
 };
